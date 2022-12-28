ec8b409685a40117a461e9b12a3dbed50413e63d
Sergey Telnov <s.telnov1993@mail.ru>
2022-12-28 11:39:40 +0300

    add login module

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,2 +1,3 @@
+export { getUserAuthData } from "./modal/selectors/getUserAuthData/getUserAuthData";
 export { userReducer, userActions } from "./modal/slice/userSlice";
 export { User, UserShema } from "./modal/types/user";
---
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -0,0 +1,41 @@
+import { LoginShema } from "./../types/loginShema";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { loginByUsername } from "../services/loginByUsername/loginByUserName";
+
+const initialState: LoginShema = {
+  username: "",
+  isLoading: false,
+  password: "",
+  error: ""
+};
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    setUserName: (state, action: PayloadAction<string>) => {
+      state.username = action.payload;
+    },
+    setPassword: (state, action: PayloadAction<string>) => {
+      state.password = action.payload;
+    }
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginByUsername.pending, (state) => {
+        state.error = undefined;
+        state.isLoading = true;
+      })
+      .addCase(loginByUsername.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(loginByUsername.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  }
+});
+
+export const { actions: loginActions } = loginSlice;
+export const { reducer: loginReducer } = loginSlice;
